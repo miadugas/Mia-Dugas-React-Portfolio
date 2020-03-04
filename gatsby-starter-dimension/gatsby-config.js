a1de134eca2c18dc2c62e8a100fdd8c316093f28
Mia Dugas <miadugas@outlook.com>
2020-03-04 10:37:03 -0700

    Mia-Dugas-React-Portfolio

--- gatsby-starter-dimension/gatsby-config.js
+++ gatsby-starter-dimension/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
+    title: 'Mia Dugas React Portfolio',
+    author: 'Mia Dugas',
     description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
   },
   plugins: [
